613de9e358154f1e8ec1debadc0c2ee63244b521
DPDmancul <davide.peressoni@tuta.io>
2022-03-16 17:21:47 +0100

    More idiomatic Haskell

--- main.hs
+++ main.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell --pure -i runghc -p "haskellPackages.ghcWithPackages (p: [ p.mwc-random ])"
+#!nix-shell --pure -i runghc -p "haskellPackages.ghcWithPackages (p: [ p.mwc-random p.pointless-fun])"
 
 --  Copyright 2022 Davide Peressoni
 --
@@ -18,82 +18,85 @@
 import System.Random.MWC
 import System.Random.MWC.Distributions
 import Control.Monad
+import Data.Function.Pointless
 
 type Point = [Double]
 newtype Cluster = UnsafeMkCluster ([Point], [(Point, Double)])
 type Clustering = [Cluster]
 
-clustering :: [[Point]] -> Int -> Double -> IO Clustering
-clustering v t δ = let k = length v in
-  sequence $ map (\x -> cluster x k t δ) v
+clustering :: Int -> Double -> [[Point]] -> IO Clustering
+clustering t δ v = sequence $ map (cluster (length v) t δ) v
 
 sil :: Clustering -> Double
 sil self = let
-    my_map :: ([a] -> a -> [a] -> b) -> [a] -> [a] -> [b]
+    my_map :: ([a] -> [a] -> a -> b) -> [a] -> [a] -> [b]
     my_map _ _ [] = []
-    my_map f prev (c:succ) = f prev c succ : my_map f (c:prev) succ
-  in sum (my_map (\prev c succ -> sum_sil c $ prev ++ succ) [] self)
-    / fromIntegral (sum $ map (\(UnsafeMkCluster(c,_)) -> length c) self)
+    my_map f prev (c:succ) = f prev succ c : my_map f (c:prev) succ
+  in sum (my_map (sum_sil .: (++)) [] self)
+    / fromIntegral (sum $ map (length . points) self)
 
-cluster :: [Point] -> Int -> Int -> Double -> IO Cluster
-cluster c k t δ = let
+cluster :: Int -> Int -> Double -> [Point] -> IO Cluster
+cluster k t δ c = let
   c_sampled = if length c <= t then return $ zip c [1, 1..] else let
-    unif = 1 / (fromIntegral $ length c)
-    prob = 2 * unif * log (2 * (fromIntegral k) / δ)
+    unif = 1 / fromIntegral (length c)
+    prob = 2 * unif * log (2 * fromIntegral k / δ)
     in do
       gen <- createSystemRandom
       let ber_gen p = bernoulli p gen
-      s_0 <- filterM (\_ -> ber_gen prob) c
-      let w_0 = map (\p -> sum $ map (d p) c) s_0
-          probs = map (\p -> min 1 (fromIntegral t * max unif (maximum $ map (\(e, w) -> d p e / w) $ zip s_0 w_0)) :: Double) c
+      s_0 <- filterM (const $ ber_gen prob) c
+      let w_0 = map (sum . flip map c . d) s_0
+          probs = map (min 1 . (fromIntegral t *) . max unif . maximum . \p -> zipWith ((/) . d p) s_0 w_0) c
       filterM (ber_gen . snd) $ zip c probs
   in UnsafeMkCluster <$> (,) c <$> c_sampled
 
-sum_sil :: Cluster -> [Cluster] -> Double
-sum_sil self@(UnsafeMkCluster (c, _)) others = sum $ map (\e -> sil_point e self others) c
+points :: Cluster -> [Point]
+points (UnsafeMkCluster (c, _)) = c
 
-w :: Cluster -> Point -> Double
-w (UnsafeMkCluster (_, self)) p = sum $ map (\(e, p_e) -> d p e / p_e) self
+sum_sil :: [Cluster] -> Cluster -> Double
+sum_sil others self = sum $ map (sil_point self others) $ points self
 
-sil_point :: Point -> Cluster -> [Cluster] -> Double
-sil_point self this_c others = let
-  a = w this_c self / fromIntegral (let UnsafeMkCluster(c, _) = this_c in length c - 1)
-  b = foldl (\m c -> min m $ w c self / fromIntegral (let UnsafeMkCluster(o, _) = c in length o))
+w :: Point -> Cluster -> Double
+w p (UnsafeMkCluster (_, self)) = sum $ map (uncurry $ (/) . d p) self
+
+sil_point :: Cluster -> [Cluster] -> Point -> Double
+sil_point c others self = let
+  a = w self c / (fromIntegral . pred . length . points) c
+  b = foldl (min .^ liftM2 (/) (w self) (fromIntegral . length . points))
     (1/0) -- infinite
     others
   in (b - a) / max a b
 
 d :: Point -> Point -> Double
-d self p = sqrt $ sum $ (**2) <$> zipWith (-) self p
+d = sqrt . sum .: zipWith ((**2) .: (-))
 
 main :: IO ()
 main = let
-  c = clustering [
+  c = clustering 4 0.1 [
         -- exact silhouette 0.19340581656247494
-                   [
-                     [1, 1],
-                     [2, 2],
-                     [3, 2],
-                     [4, 2],
-                     [-1, 3],
-                     [5, -1],
-                     [60.3, 24.5],
-                     [1, 0.5],
-                     [5, 3],
-                     [20, 21]
-                   ],
-                   [
-                     [-1, -1],
-                     [-2, -2],
-                     [-3, -2],
-                     [-4, -2],
-                     [1, -3],
-                     [-5, 1],
-                     [-60.3, -24.5],
-                     [-1, -0.5],
-                     [-5, -3],
-                     [-20, -21]
-                   ]
-                 ] 4 0.1
-  in show <$> sil <$> c >>= print
+      [
+        [1, 1],
+        [2, 2],
+        [3, 2],
+        [4, 2],
+        [-1, 3],
+        [5, -1],
+        [60.3, 24.5],
+        [1, 0.5],
+        [5, 3],
+        [20, 21]
+      ],
+      [
+        [-1, -1],
+        [-2, -2],
+        [-3, -2],
+        [-4, -2],
+        [1, -3],
+        [-5, 1],
+        [-60.3, -24.5],
+        [-1, -0.5],
+        [-5, -3],
+        [-20, -21]
+      ]
+    ]
+  in print =<< show <$> sil <$> c
 
